f20d83fb9c0ac018873c3b88dd6d79c60dbdb0e8
shaitanshamma <kurbatov_andr@hotmail.com>
2020-02-05 06:26:22 +0300

    Initial commit

---
+++ src/main/java/ru/shaitanshamma/MyAwesomeShopApplication.java
@@ -0,0 +1,13 @@
+package ru.shaitanshamma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwesomeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwesomeShopApplication.class, args);
+	}
+
+}
